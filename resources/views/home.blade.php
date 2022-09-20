b1d21be0afd815542917dda8e22218c21d779176
ChiaraMameli <mameli.chiara@libero.it>
2022-09-20 16:08:47 +0200

    print data

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,5 +8,24 @@
 </head>
 <body>
     <h1>Trains</h1>
+    <ul>
+        @foreach($trains as $train)
+            <li>
+                <h2>From: {{$train->departure_station}} | To: {{$train->arrival_station}}</h2>
+                <p>Departure Time: {{$train->departure_time}} | Arrival Time: {{$train->arrival_time}}</p>
+                <p>Train Code: {{$train->train_code}} | Carriage Number: {{$train->carriage_number}}</p>
+
+                @if($train->cancelled) 
+                    <p>CANCELLED</p> 
+                @endif
+                
+                @if(!$train->cancelled && $train->on_time) 
+                    <p>ON TIME</p>  
+                @elseif(!$train->cancelled && !$train->on_time) 
+                    <p>LATE</p> 
+                @endif
+            </li>
+        @endforeach
+    </ul>
 </body>
 </html>
